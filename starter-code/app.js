0290ffdfd110d3e616065c9c9038a15f32a0ed73
Torre <torre.locascio@gmail.com>
2018-02-20 12:44:24 -0500

    finished exercise

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,7 +8,7 @@ var app = express();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-const passportRouter = require("./routes/passportRouter");
+
 //mongoose configuration
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/passport-local");
@@ -20,16 +20,28 @@ const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const flash = require("connect-flash");
 
+//Session Requires
+
+
+const routes = require("./routes");
+
+
+
+
+
 
 
 
 
 //enable sessions here
 
+app.use(session({
+  secret: "paired programming exercise secret passport",
+  resave: true,
+  saveUninitialized: true
+}));
 
 
-
-//initialize passport and session here
 
 
 
@@ -45,10 +57,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 // require in the routers
+
 app.use('/', index);
 app.use('/', users);
-app.use('/', passportRouter);
+
+app.use('/', routes);
+
+
 
 
 
@@ -62,8 +80,44 @@ app.use('/', passportRouter);
 
 
 
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
 
 
+//initialize passport and session here
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Require Passport
+
+const passportRouter = require("./routes/passportRouter");
+
+app.use('/', passportRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
